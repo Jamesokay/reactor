6758b9affcc28d387c6269e142c3c7421417187d
James OKeeffe <jamesokeeffe50@gmail.com>
2022-02-21 22:20:06 +0800

    Basic page layout

---
+++ src/components/SideBar/SideBar.js
@@ -0,0 +1,32 @@
+import PeopleIcon from '@mui/icons-material/People';
+import GroupsIcon from '@mui/icons-material/Groups';
+import EventIcon from '@mui/icons-material/Event';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import './sideBar.css'
+
+export default function SideBar() {
+    return (
+        <div className='sideBar'>
+          <div className="sideBarListWrapper">
+            <ul className="sideBarList">
+              <li className="sideBarListItem">
+                <PeopleIcon className="sideBarIcon" />
+                <span className="sideBarListItemText">Friends</span>
+              </li>
+              <li className="sideBarListItem">
+                <GroupsIcon className="sideBarIcon" />
+                <span className="sideBarListItemText">Groups</span>
+              </li>
+              <li className="sideBarListItem">
+                <EventIcon className="sideBarIcon" />
+                <span className="sideBarListItemText">Events</span>
+              </li>
+              <li className="sideBarListItem">
+                <BookmarkIcon className="sideBarIcon" />
+                <span className="sideBarListItemText">Saved</span>
+              </li>
+            </ul>
+          </div>    
+        </div>
+    )
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,18 @@
 import NavBar from '../../components/NavBar/NavBar'
+import SideBar from '../../components/SideBar/SideBar'
+import RightBar from '../../components/RightBar/RightBar'
+import Feed from '../..//components/Feed/Feed'
+import './Home.css'
 
 export default function Home() {
     return (
-        <div>
-            <NavBar />
-        </div>
+        <>
+          <NavBar />
+          <div className='homeContainer'>
+            <SideBar />
+            <Feed />
+            <RightBar />
+          </div>
+        </>
     )
 }
