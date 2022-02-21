9c1d5b3a549c0e990ef92535914dc3da775472bf
James OKeeffe <jamesokeeffe50@gmail.com>
2022-02-21 20:01:51 +0800

    Initial commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,37 @@
+import './NavBar.css'
+import SearchIcon from '@mui/icons-material/Search'
+import PersonIcon from '@mui/icons-material/Person';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+export default function NavBar() {
+    return (
+        <div className='navBarContainer'>
+          <div className='navBarLeft'>
+              <span className='logo'>Reactor</span>
+          </div>
+          <div className='navBarCenter'>
+            <div className="searchbar">
+              <SearchIcon className='searchIcon'/>
+              <input
+                placeholder="Search Reactor"
+                className="searchInput"
+              />
+            </div>
+          </div>
+          <div className='navBarRight'>
+            <div className="topbarIcons">
+              <div className="topbarIconItem">
+                <PersonIcon />
+              </div>
+              <div className="topbarIconItem">
+                <ChatBubbleIcon />
+              </div>
+              <div className="topbarIconItem">
+                <NotificationsIcon />
+              </div>
+        </div>
+          </div>
+        </div>
+    )
+}
