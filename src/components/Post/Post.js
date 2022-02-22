27931902c41818e802a7972739983efe1cc03c47
James OKeeffe <jamesokeeffe50@gmail.com>
2022-02-22 14:30:04 +0800

    Layout for Post, including buttons and image positioning

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,11 +1,31 @@
 import './post.css'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+// import FavoriteIcon from '@mui/icons-material/Favorite';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import ShareIcon from '@mui/icons-material/Share';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+// import BookmarkIcon from '@mui/icons-material/Bookmark';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 export default function Post() {
     return (
         <div className='post'>
           <div className='postWrapper'>
-              <div className='postImg' />
-          </div>          
+            <div className='postTop'>
+              <div className='postTopLeft'>
+                <div className='profileImg' />
+                <span>Poster Name</span>
+              </div>
+                <MoreHorizIcon className='postTopIcon'/>
+            </div>
+            <div className='postImg' />
+            <div className='postBottom'>
+              <FavoriteBorderIcon className='postIcon' />
+              <ChatBubbleOutlineIcon className='postIcon' />
+              <ShareIcon className='postIcon' />
+              <BookmarkBorderIcon className='postIcon' />
+            </div>    
+          </div>      
         </div>
     )
 }
