4409c7578f43c217e22e95d7d237dbf34cbcbf47
James OKeeffe <jamesokeeffe50@gmail.com>
2022-02-22 19:58:39 +0800

    More test data

--- src/sampleData.js
+++ src/sampleData.js
@@ -1,9 +1,9 @@
 export const Posts = [
-    {
-      id: 1,
-      caption: "I've become one of those people",
-      image: "assets/posts/1.jpg",
-    },
+    // {
+    //   id: 1,
+    //   caption: "I've become one of those people",
+    //   image: "assets/posts/1.jpg",
+    // },
 
     {
       id: 2,
@@ -15,5 +15,31 @@ export const Posts = [
       id: 3,
       caption: "Fireworks!",
       image: "assets/posts/3.jpg"
+    },
+
+    {
+      id: 4,
+      caption: "A very big ferris wheel indeed",
+      image: "assets/posts/4.jpg"
+    },
+
+    {
+      id: 5,
+      caption: "Hostel vibes",
+      image: "assets/posts/5.jpg"
+    },
+
+    // {
+    //   id: 6,
+    //   caption: "Aquarium",
+    //   image: "assets/posts/6.jpg"
+    // },
+
+    {
+      id: 7,
+      caption: "Osaka by night",
+      image: 'assets/posts/7.jpg'
     }
+
+
 ]
