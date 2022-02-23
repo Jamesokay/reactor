b5de402408a7c547703ec1e5599fb7a9f1721cbe
James OKeeffe <jamesokeeffe50@gmail.com>
2022-02-23 09:28:01 +0800

    Added compressed profile images

--- src/components/RightBar/RightBar.js
+++ src/components/RightBar/RightBar.js
@@ -5,10 +5,11 @@ export default function RightBar() {
     return (
         <div className='rightBar'>
           <div className='rightBarWrapper'>
-            <h4 className="rightBarTitle">Online Friends</h4>
+            <h4 className="rightBarTitle">Online</h4>
             <ul className="rightBarFriendsList">
               {Users.map((u) => (
                 <li className="rightbarFriend" key={u.id}>
+                  <img className="rightbarProfileImg" src={u.compImg} alt="" />
                   <span className="rightbarUsername">{u.name}</span>
                 </li>
               ))}
--- src/sampleData.js
+++ src/sampleData.js
@@ -20,43 +20,42 @@ export const Users = [
     {
       id: 1,
       profileImg: "assets/users/u1.jpg",
+      compImg: "assets/users/u1comp.jpg",
       name: "Benjamin Lawrence"
      },
 
     {
       id: 2,
       profileImg: "assets/users/u2.jpg",
+      compImg: "assets/users/u2comp.jpg",
       name: "Eric Choi"
     },
 
     {
       id: 3,
       profileImg: "assets/users/u3.jpg",
+      compImg: "assets/users/u3comp.jpg",
       name: "Thom Kingsley"
     },
 
     {
       id: 4,
       profileImg: "assets/users/u4.jpg",
+      compImg: "assets/users/u4comp.jpg",
       name: "Cara Zillman"
     },
 
     {
       id: 5,
       profileImg: "assets/users/u5.jpg",
+      compImg: "assets/users/u5comp.jpg",
       name: "Laura Lehman"
     },
 
     {
       id: 6,
       profileImg: "assets/users/u6.jpg",
+      compImg: "assets/users/u6comp.jpg",
       name: "Mira Jukes"
-    },
-
-    {
-      id: 7,
-      profileImg: "assets/users/u7.jpg",
-      name: "Hannah Gilbert"
     }
-
 ]
