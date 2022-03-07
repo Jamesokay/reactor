83b9a177715fa5d6930e0c8711e42c51f7f7259b
James OKeeffe <jamesokeeffe50@gmail.com>
2022-03-07 13:02:39 +0800

    Added basic like/unlike functionality

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -1,14 +1,18 @@
 import './post.css'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { Link } from "react-router-dom"
 import { useEffect, useState, useContext } from 'react'
 import axios from 'axios'
 import { PostContext } from '../../context/PostContext';
+import { AuthContext } from '../../context/AuthContext';
 
 export default function Post({ post }) {
   const [user, setUser] = useState({})
+  const { userObject } = useContext(AuthContext)
   const { setPostObject } = useContext(PostContext)
+  const [isLiked, setIsLiked] = useState(post.likes.includes(userObject.user._id))
 
   useEffect(() => {
     
@@ -48,6 +52,16 @@ export default function Post({ post }) {
     setPostObject({...post, postAuthor: user})
   }
 
+  const handleLike = async () => {
+    try {
+      const res = await axios.put('/posts/' + post._id + '/like', { userId: userObject.user._id })
+      console.log(res)
+    } catch (err) {
+      console.error(err)
+    }
+    setIsLiked(!isLiked)
+  }
+
 
 
 
@@ -64,9 +78,13 @@ export default function Post({ post }) {
                 <Link to={`/profile/${user.username}`} className='profileName'>{user.username}</Link>
               </div>
               <div className='postBottomRight'>
+              {(isLiked)?
+                  <FavoriteIcon onClick={() => handleLike()} style={{color: 'red'}}/>
+                :
                 <div className='postIcon'>
-                  <FavoriteBorderIcon />
+                  <FavoriteBorderIcon onClick={() => handleLike()}/>
                 </div>
+              }
                 <div className='postIcon'>
                   <MoreVertIcon onClick={(e) => deletePost(e)}/>
                 </div>
