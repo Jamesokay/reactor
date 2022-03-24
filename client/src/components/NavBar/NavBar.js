4b36e394d2b9476876b946ad36bfcca43c643f81
James OKeeffe <jamesokeeffe50@gmail.com>
2022-03-24 09:27:57 +0800

    Added dropdown menu to NavBar with logout option

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,7 +1,9 @@
 import './NavBar.css'
-// import LogoutIcon from '@mui/icons-material/Logout';
-import { Link } from "react-router-dom"
-import { useContext } from 'react'
+import LogoutIcon from '@mui/icons-material/Logout';
+import SettingsIcon from '@mui/icons-material/Settings';
+import PersonIcon from '@mui/icons-material/Person';
+import { useNavigate, Link } from "react-router-dom"
+import { useContext, useState } from 'react'
 import { AuthContext } from '../../context/AuthContext'
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search'
@@ -10,13 +12,14 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 export default function NavBar() {
-    const { userObject } = useContext(AuthContext)
-//    const navigate = useNavigate()
+    const { userObject, setUserObject } = useContext(AuthContext)
+    const [menuVisible, setMenuVisible] = useState(false)
+    const navigate = useNavigate()
 
-    // const logOut = () => {
-    //   setUserObject({user: null, isFetching: false, error: false})
-    //   navigate('/')
-    // }
+    const logOut = () => {
+      setUserObject({user: null, isFetching: false, error: false})
+      navigate('/')
+    }
 
     return (
         <div className='navBarContainer'>
@@ -43,8 +46,26 @@ export default function NavBar() {
           <div className='navBarRight'>
               <img className='navBarImg' src={userObject.user.profilePicture} alt='' />
               <div className='navBarIconItem'>
-                <KeyboardArrowDownIcon />
+                <KeyboardArrowDownIcon onClick={() => setMenuVisible(!menuVisible)}/>
               </div>
+            {menuVisible && (
+              <div className='navMenuContainer'>
+                <ul className='navMenu'>
+                  <li className='navMenuOption'>
+                    <PersonIcon className='navMenuIcon' />
+                    <span>Edit Profile</span>
+                  </li>
+                  <li className='navMenuOption'>
+                    <SettingsIcon className='navMenuIcon' />
+                    <span>Account Settings</span>
+                  </li>
+                  <li className='navMenuOption' onClick={() => logOut()}>
+                    <LogoutIcon className='navMenuIcon' />
+                    <span>Log Out</span>
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
         </div>
     )
