0b967efe125015c71a63f5c46c99519b3d883b62
James OKeeffe <jamesokeeffe50@gmail.com>
2022-04-06 09:34:31 +0800

    Modification to tag logic

--- api/controller/appController.js
+++ api/controller/appController.js
@@ -7,7 +7,7 @@ const uploadPost = async (req, res) => {
       const newPost = new Post({
         userId: req.body.userId,
         img: req.file.path,
-        tags: JSON.parse(req.body.tags),
+        tags: req.body.tags? JSON.parse(req.body.tags): [],
         desc: req.body.caption
       })
       await newPost.save();
